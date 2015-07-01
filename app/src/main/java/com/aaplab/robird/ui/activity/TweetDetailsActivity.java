7878b09bc1ee09601904a72a64b4a3ce3676dbab
majid <cmecid@gmail.com>
2015-07-01 17:53:11 +0500

    Tweet Details activity refactored

--- app/src/main/java/com/aaplab/robird/ui/activity/TweetDetailsActivity.java
+++ app/src/main/java/com/aaplab/robird/ui/activity/TweetDetailsActivity.java
@@ -7,23 +7,34 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.aaplab.robird.R;
 import com.aaplab.robird.data.entity.Account;
 import com.aaplab.robird.data.entity.Tweet;
 import com.aaplab.robird.ui.fragment.TweetDetailsFragment;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by majid on 21.06.15.
  */
-public class TweetDetailsActivity extends BaseActivity {
+public class TweetDetailsActivity extends BaseActivity implements View.OnClickListener {
+
+    @InjectView(R.id.fab)
+    FloatingActionButton mFloatingActionButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_with_fab);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ((FloatingActionButton) findViewById(R.id.fab)).setImageResource(R.drawable.ic_reply);
+        ButterKnife.inject(this);
+
+        mFloatingActionButton.setImageResource(R.drawable.ic_reply);
+        mFloatingActionButton.setOnClickListener(this);
 
         if (savedInstanceState == null) {
             final Account account = (Account) getIntent().getSerializableExtra("account");
@@ -48,5 +59,10 @@ public class TweetDetailsActivity extends BaseActivity {
         intent.putExtra("account", account);
         intent.putExtra("tweet", tweet);
         ActivityCompat.startActivity(activity, intent, null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO start compose screen
     }
 }
