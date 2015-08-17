4987b982e25c4a12894a10aecf8d99274090ee85
majid <cmecid@gmail.com>
2015-08-17 20:23:58 +0500

    Floating Compose button added for list timeline.

--- app/src/main/java/com/aaplab/robird/ui/activity/UserListActivity.java
+++ app/src/main/java/com/aaplab/robird/ui/activity/UserListActivity.java
@@ -4,20 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.aaplab.robird.R;
 import com.aaplab.robird.data.entity.Account;
 import com.aaplab.robird.data.entity.UserList;
+import com.aaplab.robird.ui.fragment.ComposeFragment;
 import com.aaplab.robird.ui.fragment.TimelineFragment;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by majid on 17.08.15.
  */
-public class UserListActivity extends BaseActivity {
+public class UserListActivity extends BaseActivity implements View.OnClickListener {
+
+    @Bind(R.id.fab)
+    FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +38,8 @@ public class UserListActivity extends BaseActivity {
         final UserList userList = getIntent().getParcelableExtra("list");
 
         setTitle(userList.name());
+        mFloatingActionButton.setOnClickListener(this);
+        mFloatingActionButton.setImageResource(R.drawable.ic_edit);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -39,6 +48,12 @@ public class UserListActivity extends BaseActivity {
                             TimelineFragment.create(account, userList.listId()))
                     .commit();
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        ComposeFragment.create((Account) getIntent().getParcelableExtra("account"))
+                .show(getSupportFragmentManager(), ComposeFragment.TAG_COMPOSE);
     }
 
     public static void start(Activity activity, Account account, UserList userList) {
