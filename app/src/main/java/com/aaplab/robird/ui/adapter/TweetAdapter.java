40569f89c4cb2bd187dd39ce486778f1373a2eb7
majid <cmecid@gmail.com>
2015-08-23 21:21:10 +0500

    Multiple image counter implemented.

--- app/src/main/java/com/aaplab/robird/ui/adapter/TweetAdapter.java
+++ app/src/main/java/com/aaplab/robird/ui/adapter/TweetAdapter.java
@@ -70,10 +70,13 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetHolder>
 
         holder.infoTextView.setText(information.toString());
         holder.retweetImageView.setVisibility(TextUtils.isEmpty(tweet.retweetedBy()) ? View.GONE : View.VISIBLE);
-        holder.mediaImageView.setVisibility(TextUtils.isEmpty(tweet.media()) ? View.GONE : View.VISIBLE);
 
-        if (holder.mediaImageView.getVisibility() == View.VISIBLE) {
+        if (!TextUtils.isEmpty(tweet.media())) {
             final String[] media = tweet.media().split("\\+\\+\\+\\+\\+");
+
+            holder.mediaCountTextView.setVisibility(media.length > 1 ? View.VISIBLE : View.GONE);
+            holder.mediaCountTextView.setText("" + media.length);
+            holder.mediaImageView.setVisibility(View.VISIBLE);
 
             Glide.with(mActivity)
                     .load(media[0])
@@ -86,6 +89,9 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetHolder>
                     ImagesActivity.start(mActivity, media);
                 }
             });
+        } else {
+            holder.mediaCountTextView.setVisibility(View.GONE);
+            holder.mediaImageView.setVisibility(View.GONE);
         }
 
         Glide.with(mActivity)
@@ -121,6 +127,9 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetHolder>
 
         @Bind(R.id.media)
         ImageView mediaImageView;
+
+        @Bind(R.id.count)
+        TextView mediaCountTextView;
 
         @Bind(R.id.retweet)
         ImageView retweetImageView;
