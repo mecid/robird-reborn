1d2d8886584b82883b1cdaeffe6b04f1d3df273a
majid <cmecid@gmail.com>
2015-09-11 16:07:51 +0500

    Timeline font size settings UI implemented.

--- app/src/main/java/com/aaplab/robird/data/model/PrefsModel.java
+++ app/src/main/java/com/aaplab/robird/data/model/PrefsModel.java
@@ -9,10 +9,15 @@ import com.aaplab.robird.inject.Inject;
  */
 public final class PrefsModel {
     public static final String PREFER_DARK_THEME = "prefer_dark_theme";
+    public static final String TIMELINE_FONT_SIZE = "font_size";
 
     private final SharedPreferences mPreferences = Inject.preferences();
 
     public boolean isDarkTheme() {
         return mPreferences.getBoolean(PREFER_DARK_THEME, false);
+    }
+
+    public int fontSize() {
+        return Integer.parseInt(mPreferences.getString(TIMELINE_FONT_SIZE, "14"));
     }
 }
