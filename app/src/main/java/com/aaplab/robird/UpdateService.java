207259d3c26e963634c0361b388440b255f44e64
majid <cmecid@gmail.com>
2015-09-13 02:14:57 +0500

    Background service exception handling added.

--- app/src/main/java/com/aaplab/robird/UpdateService.java
+++ app/src/main/java/com/aaplab/robird/UpdateService.java
@@ -9,6 +9,7 @@ import com.aaplab.robird.data.model.AccountModel;
 import com.aaplab.robird.data.model.DirectsModel;
 import com.aaplab.robird.data.model.TimelineModel;
 import com.aaplab.robird.data.model.UserListsModel;
+import com.aaplab.robird.util.DefaultObserver;
 
 import java.util.List;
 
@@ -24,17 +25,21 @@ public final class UpdateService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+
         for (Account account : new AccountModel().accounts().toBlocking().first()) {
-            new TimelineModel(account, TimelineModel.HOME_ID).update().subscribe();
-            new TimelineModel(account, TimelineModel.MENTIONS_ID).update().subscribe();
-            new TimelineModel(account, TimelineModel.RETWEETS_ID).update().subscribe();
-            new TimelineModel(account, TimelineModel.FAVORITES_ID).update().subscribe();
-            new UserListsModel(account).update().subscribe();
-            new DirectsModel(account).update().subscribe();
+            new TimelineModel(account, TimelineModel.HOME_ID).update().subscribe(OBSERVER);
+            new TimelineModel(account, TimelineModel.MENTIONS_ID).update().subscribe(OBSERVER);
+            new TimelineModel(account, TimelineModel.RETWEETS_ID).update().subscribe(OBSERVER);
+            new TimelineModel(account, TimelineModel.FAVORITES_ID).update().subscribe(OBSERVER);
+            new UserListsModel(account).update().subscribe(OBSERVER);
+            new DirectsModel(account).update().subscribe(OBSERVER);
 
             List<UserList> userLists = new UserListsModel(account).lists().toBlocking().first();
             for (UserList userList : userLists)
-                new TimelineModel(account, userList.listId()).update().subscribe();
+                new TimelineModel(account, userList.listId()).update().subscribe(OBSERVER);
         }
     }
+
+    private static final DefaultObserver<Integer> OBSERVER = new DefaultObserver<Integer>() {
+    };
 }
