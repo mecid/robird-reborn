41b9fb2ad15976cec9586c052ea8ad51e376418d
majid <cmecid@gmail.com>
2015-09-23 14:49:35 +0500

    Image saving implemented.

--- app/src/main/java/com/aaplab/robird/ui/fragment/ImageFragment.java
+++ app/src/main/java/com/aaplab/robird/ui/fragment/ImageFragment.java
@@ -1,14 +1,24 @@
 package com.aaplab.robird.ui.fragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.aaplab.robird.R;
+import com.aaplab.robird.inject.Inject;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 /**
  * Created by majid on 23.08.15.
@@ -26,16 +36,41 @@ public class ImageFragment extends BaseFragment {
     }
 
     private ImageView mImageView;
+    private String mImage;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        final String image = getArguments().getString("image");
+        mImage = getArguments().getString("image");
 
         Glide.with(this)
-                .load(image)
+                .load(mImage)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(mImageView);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.image, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_save) {
+            Glide.with(this).load(mImage).asBitmap().into(new SimpleTarget<Bitmap>() {
+                @Override
+                public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                    MediaStore.Images.Media.insertImage(Inject.contentResolver(), resource, "", "");
+                    Snackbar.make(getActivity().findViewById(R.id.coordinator),
+                            R.string.image_saved, Snackbar.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Nullable
