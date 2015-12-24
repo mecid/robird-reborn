549d8665d3bb4c96c8a252c54837d8d5f11c33a0
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-12-24 15:30:41 +0100

    Working on status removal logic.

--- app/src/main/java/com/aaplab/robird/data/model/StreamModel.java
+++ app/src/main/java/com/aaplab/robird/data/model/StreamModel.java
@@ -3,70 +3,61 @@ package com.aaplab.robird.data.model;
 import android.content.ContentValues;
 import android.net.Uri;
 
+import com.aaplab.robird.data.MapFunctions;
+import com.aaplab.robird.data.SqlBriteContentProvider;
 import com.aaplab.robird.data.entity.Account;
 import com.aaplab.robird.data.entity.Tweet;
 import com.aaplab.robird.data.provider.contract.TweetContract;
 import com.aaplab.robird.inject.Inject;
+
+import java.util.List;
 
 import timber.log.Timber;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
-import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
 public final class StreamModel extends BaseTwitterModel {
-    private TwitterStream mTwitterStream;
+    private final TwitterStream mTwitterStream;
+    private final SqlBriteContentProvider mSqlBriteContentProvider;
 
     public StreamModel(Account account) {
         super(account);
 
-        try {
-            mTwitterStream = new TwitterStreamFactory()
-                    .getInstance(mTwitter.getOAuthAccessToken());
-        } catch (TwitterException e) {
-            Timber.d(e, "Access token is not available or expired");
-        }
+        mTwitterStream = new TwitterStreamFactory().getInstance(mTwitter.getAuthorization());
+        mSqlBriteContentProvider = SqlBriteContentProvider.create(Inject.contentResolver());
     }
 
     public void start() {
-        System.out.println("Called");
-
         if (mTwitterStream != null) {
-            mTwitterStream.addListener(new UserStatusListener(mAccount));
+            mTwitterStream.addListener(new UserStatusListener());
             mTwitterStream.user();
         }
     }
 
     public void stop() {
         if (mTwitterStream != null) {
+            // remove reference to listener
             mTwitterStream.clearListeners();
+            // shutdown stream consuming thread
             mTwitterStream.cleanUp();
-            mTwitterStream.shutdown();
         }
     }
 
-    private static class UserStatusListener implements StatusListener {
-        private final Account account;
-
-        public UserStatusListener(Account account) {
-            this.account = account;
-        }
+    private class UserStatusListener implements StatusListener {
 
         @Override
         public void onStatus(Status status) {
+            System.out.println("AccountID: " + status.getUser().getId());
             System.out.println("Status.text: " + status.getText());
-            System.out.println("Scopes: " + status.getScopes());
-            for (String id : status.getScopes().getPlaceIds()) {
-                System.out.println("ID: " + id);
-            }
 
             Tweet tweet = Tweet.from(status);
 
             ContentValues contentValues = tweet.toContentValues();
-            contentValues.put(TweetContract.ACCOUNT_ID, account.id());
+            contentValues.put(TweetContract.ACCOUNT_ID, mAccount.id());
             contentValues.put(TweetContract.TIMELINE_ID, 1);
 
             Uri uri = Inject.contentResolver().insert(TweetContract.CONTENT_URI, contentValues);
@@ -75,7 +66,32 @@ public final class StreamModel extends BaseTwitterModel {
 
         @Override
         public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            System.out.println("StatusRemoved: " + statusDeletionNotice.getStatusId() +
+                    "\n" + "UserID: " + statusDeletionNotice.getUserId());
 
+            List<Tweet> persistedTweets = mSqlBriteContentProvider
+                    .query(TweetContract.CONTENT_URI, TweetContract.PROJECTION,
+                            String.format("%s=%d AND %s=%d",
+                                    TweetContract.TWEET_ID, statusDeletionNotice.getStatusId(),
+                                    TweetContract.ACCOUNT_ID, mAccount.id()),
+                            null, null, false)
+                    .map(MapFunctions.TWEET_LIST)
+                    .toBlocking()
+                    .first();
+
+            if (persistedTweets != null && persistedTweets.size() > 0) {
+                Tweet persistedTweet = persistedTweets.get(0);
+
+                Timber.d(String.format("Deleting tweet \"%s\" with id=%d",
+                        persistedTweet.text(),
+                        persistedTweet.tweetId()));
+
+                Uri persistedTweetUri = Uri.withAppendedPath(TweetContract.CONTENT_URI,
+                        String.valueOf(persistedTweet.id()));
+                Integer status = mSqlBriteContentProvider
+                        .delete(persistedTweetUri, null, null).toBlocking().first();
+                System.out.println("Tweet removal status: " + status);
+            }
         }
 
         @Override
