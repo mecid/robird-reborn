51cefeeb712afdc60faafabb0bcc59161267f3e0
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-02-16 00:09:19 +0100

    Updating Timelines before enabling streaming.

--- app/src/main/java/com/aaplab/robird/ui/fragment/StreamFragment.java
+++ app/src/main/java/com/aaplab/robird/ui/fragment/StreamFragment.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import com.aaplab.robird.data.entity.Account;
 import com.aaplab.robird.data.model.AccountModel;
 import com.aaplab.robird.data.model.StreamModel;
+import com.aaplab.robird.data.model.TimelineModel;
 import com.aaplab.robird.util.DefaultObserver;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.Subscriptions;
 
@@ -39,15 +42,28 @@ public final class StreamFragment extends Fragment {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .take(1)
-                .subscribe(new DefaultObserver<List<Account>>() {
+                .flatMap(new Func1<List<Account>, Observable<Account>>() {
                     @Override
-                    public void onNext(List<Account> accounts) {
-                        for (Account account : accounts) {
-                            StreamModel streamModel = new StreamModel(account);
-                            streamModel.start();
+                    public Observable<Account> call(List<Account> accounts) {
+                        return Observable.from(accounts);
+                    }
+                })
+                .map(new Func1<Account, StreamModel>() {
+                    @Override
+                    public StreamModel call(Account account) {
+                        (new TimelineModel(account, TimelineModel.HOME_ID)).update().toBlocking();
+                        (new TimelineModel(account, TimelineModel.MENTIONS_ID)).update().toBlocking();
+                        (new TimelineModel(account, TimelineModel.RETWEETS_ID)).update().toBlocking();
+                        (new TimelineModel(account, TimelineModel.FAVORITES_ID)).update().toBlocking();
 
-                            mStreamModels.add(streamModel);
-                        }
+                        return new StreamModel(account);
+                    }
+                })
+                .subscribe(new DefaultObserver<StreamModel>() {
+                    @Override
+                    public void onNext(StreamModel streamModel) {
+                        mStreamModels.add(streamModel);
+                        streamModel.start();
                     }
                 });
     }
